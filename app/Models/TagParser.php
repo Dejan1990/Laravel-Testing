8cf3be6410a23bdd2f6aadf0c7eac582f3c5fe2e
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-10-27 16:51:16 +0200

    Example-1

---
+++ app/Models/TagParser.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TagParser extends Model
+{
+    use HasFactory;
+
+    public function parse(string $tags): array
+	{
+		return preg_split("/ ?[,|] ?/", $tags); //upitnik znaci da je preshodni karakter opcioni - u ovom slucaju to je razmak
+		//[,|] znaci da je dozvoljeno i , i |
+	}
+}
